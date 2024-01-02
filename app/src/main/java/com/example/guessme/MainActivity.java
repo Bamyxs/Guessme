86bdf45163479df16bbf23810182971e2c162631
Nicole Vero <118127323+Bamyxs@users.noreply.github.com>
2024-01-02 20:59:46 +0800

    Final of my project

--- app/src/main/java/com/example/guessme/MainActivity.java
+++ app/src/main/java/com/example/guessme/MainActivity.java
@@ -71,7 +71,7 @@ public class MainActivity extends AppCompatActivity {
                     Toast.makeText(MainActivity.this, "CONGRATULATIONS!", Toast.LENGTH_SHORT).show();
                 } else {
                     resultTextView.setText("Incorrect. Try again.");
-                    Toast.makeText(MainActivity.this, "Try again!", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, "TRY AGAIN!", Toast.LENGTH_SHORT).show();
                 }
             } else {
                 resultTextView.setText("Please enter a color name.");
